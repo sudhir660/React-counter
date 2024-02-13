57c3c24ef0feab34538f4b33f3e0902201fff149
sudhir_sherkar <sudhirsherkar660@gmail.com>
2024-02-13 11:13:08 +0530

    react couter done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+// Filename - App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+	
+	const [counter, setCounter] = useState(0);
+
+
+	const handleClick1 = () => {
+		setCounter(counter + 1);
+    console.log('state is changed');
+	};
+
+
+	const handleClick2 = () => {
+		setCounter(counter - 1);
+	};
+
+	return (
+    
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+				fontSize: "300%",
+				position: "absolute",
+				width: "100%",
+				height: "100%",
+				// top: "-15%",
+			}}
+		>
+			Counter App
+			<div className="counter"
+				style={{
+					fontSize: "120%",
+					position: "relative",
+					top: "10vh",
+				}}
+			>
+				{counter}
+			</div>
+			<div className="buttons">
+      <button
+					style={{
+						fontSize: "60%",
+						position: "relative",
+						top: "20vh",
+						margin: "5px",
+						backgroundColor: "red",
+						borderRadius: "30%",
+						color: "white",
+            border:'none',
+           
+					}}
+					onClick={handleClick2}
+				>
+					Decre
+				</button>
+        
+				<button
+					style={{
+						fontSize: "60%",
+						position: "relative",
+						top: "20vh",
+						marginRight: "5px",
+						backgroundColor: "green",
+						borderRadius: "30%",
+						color: "white",
+            border:'none'
+					}}
+					onClick={handleClick1}
+				>
+					Add
+				</button>
+			
+			</div>
+		</div>
+	);
+};
 
 export default App;
